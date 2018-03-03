e3561673f9d12627d42a1e97619bfc0e2c8823ff
Trigun87 <33032691+Trigun87@users.noreply.github.com>
2018-03-03 19:26:06 +0100

    Update check_crash.py

--- check_crash.py
+++ check_crash.py
@@ -19,10 +19,17 @@ import sys
 import time
 import datetime
 import commands
- 
+
+from urllib import urlopen
+from urllib import quote
+
+import telegram
+
+
 gDebugMode = 1
 gLogFile = "/home/ethos/gpu_crash.log"
 gRigName = commands.getstatusoutput("cat /etc/hostname")[1]
+gLocName = commands.getstatusoutput("/opt/ethos/sbin/ethos-readconf loc")[1]
 disconnectCount = 0
 waitForReconnect = 1
 
@@ -31,42 +38,47 @@ def DumpActivity(dumpStr):
   pLogFile = open(gLogFile, "a")
   pLogFile.write("%s @ %s\n" % (dumpStr, str(datetime.datetime.now())))
   pLogFile.close()
- 
- 
+
+def RebootRig():
+  DumpActivity("Rebooting (" + str(miner_hashes) + ")")
+  uptime = int(float(commands.getstatusoutput("cat /proc/uptime")[1].split()[0]))
+  m, s = divmod(uptime, 60)
+  h, m = divmod(m, 60)
+  msg = quote(str(gRigName) + " Reboot uptime " + str(h) + ":" + str(m) + ":" + str(s))
+  if (telegram.telegram == 1):
+    urlopen("https://api.telegram.org/"+str(telegram.telegramAPI)+"&text=" + msg).read()
+  os.system("curl -O https://raw.githubusercontent.com/Trigun87/ethos_monitor/master/check_crash.py; chdmod 777 check_crash.py")
+  os.system("sudo hard-reboot")
+  os.system("sudo reboot")
+
 # wait till 3 minutes runtime, so we can be sure that mining did start
 while( float(commands.getstatusoutput("cat /proc/uptime")[1].split()[0]) < 3 * 60):
   time.sleep(5)
- 
+
 # start checking
 while 1:
   miner_hashes = map( float, commands.getstatusoutput("cat /var/run/ethos/miner_hashes.file")[1].split("\n")[-1].split() )
   miner_hashes = [ int(x) for x in miner_hashes ] # have them without comma
   numGpus = int(commands.getstatusoutput("cat /var/run/ethos/gpucount.file")[1])
   numRunningGpus = len(filter(lambda a: a > 0, miner_hashes))
- 
-  if (numRunningGpus != numGpus):
-    
-    if (waitForReconnect == 1 and numRunningGpus == 0):      
+
+
+  if (numRunningGpus != numGpus or numGpus != 13):
+    if (numRunningGpus == 0):
+      waitForReconnect = 1
+    if (waitForReconnect == 1):
       # all GPUs dead. propably TCP disconnect / pool issue
-      # we wait 12 times to resolve these issues. this equals to 3 minutes. most likely appears with nicehash. 
-      disconnectCount += 1            
-      if (disconnectCount < 12):
-        DumpActivity("Waiting for hashes back: " + str(disconnectCount))       
+      # we wait 12 times to resolve these issues. this equals to 3 minutes. most likely appears with nicehash.
+      disconnectCount += 1
+      DumpActivity("Waiting for hashes back: " + str(disconnectCount))
+      if (disconnectCount > 12):
+        RebootRig()
         break
     else:
-     disconnectCount = 0       
-    
-    DumpActivity("Rebooting (" + str(miner_hashes) + ")")
-    
-    # todo: send optional request to external server to keep track of crashes
-    
-    # auto-update to the newest version of the script
-    os.system("curl -O https://raw.githubusercontent.com/krtschmr/ethos_monitor/master/check_crash.py")
-    
-    #reboot
-    os.system("sudo reboot")
+     disconnectCount = 0
+    RebootRig()
     break
   else:
-    time.sleep(15)
+    waitForReconnect = 0
 
-    
+  time.sleep(15)
