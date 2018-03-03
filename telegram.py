edd02d040452340d8308384291cc167ff90bef29
Trigun87 <33032691+Trigun87@users.noreply.github.com>
2018-03-03 19:21:42 +0100

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+#change telegram = 1 if you want use telegram
 telegram = 0
 #change with ur bot api and ur id like "bot123456789:apiKeyFor_the_botBYFatherBot123/sendmessage?chat_id=12345678"
 telegramAPI = "botXXXXXXX:XXXBOTAPIXXXX/sendmessage?chat_id=XXXIDXXX"
