fe73a0d9c785081edb2c3cab77e32f2d1877e253
Trigun87 <33032691+Trigun87@users.noreply.github.com>
2018-03-03 19:21:12 +0100

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+telegram = 0
+#change with ur bot api and ur id like "bot123456789:apiKeyFor_the_botBYFatherBot123/sendmessage?chat_id=12345678"
+telegramAPI = "botXXXXXXX:XXXBOTAPIXXXX/sendmessage?chat_id=XXXIDXXX"
+
+
