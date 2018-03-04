d389d0b78980f9aa827a349228ef2f09e70365ee
Trigun87 <33032691+Trigun87@users.noreply.github.com>
2018-03-04 20:33:46 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+#number of gpu in your rig, if less reboot
+GpuNum = 13
 #change telegram = 1 if you want use telegram
 telegram = 0
 #change with ur bot api and ur id like "bot123456789:apiKeyFor_the_botBYFatherBot123/sendmessage?chat_id=12345678"
